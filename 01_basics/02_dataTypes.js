3368eb2c2ca8b5b1bde6cd723d1c5b7e7926d81f
Dipali Chormale <116097814+dipalichormale@users.noreply.github.com>
2024-06-20 09:16:14 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using nodejs, not brower
+
+let name = "dipu"
+let age = 22
+let isLoggedIn = true
+let state;
+
+
+// number  => 2 to power 53
+// bigint
+//string =>""
+// boolean => true/false
+//null => standalone value
+//undefined => is null
+//symbol => when use react then use it in components, to find out uniquess of components symbol is udseful
+
+
+// object
+
+console.log(typeof undefined); //undefined
+console.log(typeof null); // object
+
