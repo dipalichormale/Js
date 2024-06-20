8c4ce7a2344d3691b9a20fad025cf859751751c2
Dipali Chormale <116097814+dipalichormale@users.noreply.github.com>
2024-06-20 10:20:29 +0000

    Operation

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -22,5 +22,40 @@ let booleanIsLoggedin = Boolean(isLoggedIn)
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+//***********************Operation**********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+let str1 = "hello"
+let str2 = " dipu"
+
+let str3 = str1+str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2); 
+// console.log(1 + 2 + "2"); 
+
+// console.log(+true);
+// console.log(+"");
+
+
+let gmaeCounter = 100
+gmaeCounter++; // => postfix;    //++gameCounter => prefix
+console.log(gmaeCounter);
+
+
