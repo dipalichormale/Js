6999d3fad7a6a863e6f380d3fba712c88b49b1dd
Dipali Chormale <116097814+dipalichormale@users.noreply.github.com>
2024-06-20 09:53:34 +0000

    conversion of datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,7 +1,26 @@
-let score = "33"
+let score = true
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
 let valueInNumber = Number(score)
-console.log(typeof valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => 33
+//"33abc" => NaN
+//true => 1; false =>0
+
+let isLoggedIn = "Dipali"
+
+let booleanIsLoggedin = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedin);
+
+//1 => true; 0 => false
+//"" =>flase
+//"dipali" => true
+
+let someNumber = 33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
