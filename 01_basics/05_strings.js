85118ff06bad67d89d7cf98481cb56701c1c5782
Dipali Chormale <116097814+dipalichormale@users.noreply.github.com>
2024-06-21 16:08:35 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,41 @@
+const name = "dipali"
+const Sname = " chormale"
+
+// console.log(name + Sname+ " 22");
+
+// console.log(`${name}${Sname}`);
+
+const gameName = new String("dipali-dc")
+let index= 2
+
+// console.log(gamaeName[0]);
+// console.log(gamaeName.__proto__);
+
+// console.log(gamaeName.length);
+// console.log(gamaeName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('a'));
+
+// const newString = gameName.substring(0,4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8,4)
+// console.log(anotherString);
+
+// const newStringOne = "    dipali    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://dipali.com/dipali%20chormale"
+
+// console.log(url.replace('%20','-'));
+// console.log(url.includes('dipali'));
+
+// console.log(gameName.split('-'));
+console.log(gameName.valueOf());
+
+
+
+
+
+
