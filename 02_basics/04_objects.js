4346dc71abc45710927b352f12ceaa4c0ffb4ad3
Dipali Chormale <116097814+dipalichormale@users.noreply.github.com>
2024-06-24 09:38:39 +0000

    object2

---
+++ 02_basics/04_objects.js
@@ -0,0 +1,63 @@
+// singleton or using contructor
+
+// const tinderuser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "1234"
+tinderUser.name = "dipali"
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email:"dd@gmail.com",
+    fullname:{
+        userfullname:{ 
+            firstname: "dipali",
+            lastname: "chormale"
+        }
+    }
+}
+
+// console.log(regularUser.fullname.userfullname.firstname)
+
+const obj1 = {1:"a", 2: "b"}
+const obj2 = {3:"a", 4: "b"}
+const obj4 = {5:"a", 6: "b"}
+
+
+// const obj3 = {obj1, obj2}
+// const obj3 =  Object.assign({}, obj1, obj2, obj4)
+
+const obj3 = {...obj1, ...obj2}
+// console.log(obj3);
+
+// array object
+
+const user = [
+    {
+        email:"d@gmail.com",
+        name: "dipa"
+    },
+    {
+        email:"d@gmail.com",
+        name: "dipa"
+    },
+    {
+        email:"d@gmail.com",
+        name: "dipa"
+    },
+    {
+        email:"d@gmail.com",
+        name: "dipa"
+    }
+]
+
+// user[1].email
+
+console.log(tinderUser);
+console.log(Object.keys(tinderUser));
+console.log(Object.keys(tinderUser));
+console.log(Object.entries(tinderUser));
+
+console.log(tinderUser.hasOwnProperty('id'));
+
