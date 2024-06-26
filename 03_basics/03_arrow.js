4821af9e29dc39817c204f8046d243fbb5550e10
Dipali Chormale <116097814+dipalichormale@users.noreply.github.com>
2024-06-26 09:16:23 +0000

    arrow function

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -3,6 +3,53 @@ const user = {
     price : 999,
 
     welcomeMessage : function () {
-        console.log(`${this.username}, welcomr to website`);
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
     }
  } 
+
+//  user.welcomeMessage()
+//  user.username = " sam"
+//  user.welcomeMessage()
+
+// console.log(this);
+
+// function chai(){
+//     let username = "dipali"
+//     console.log(this.username);
+// }
+
+// chai()
+
+// const chai = function(){
+//     let username = "dipali"
+//     console.log(this.username);
+// }
+// chai()
+
+const chai = () => {
+    let username = "dipali"
+    console.log(this);
+}
+// chai()
+
+
+// explicit return(we use return keyword)
+// const addTwo = (num1, num2) => {
+// return num1+num2
+// }
+// console.log(addTwo(2,3));
+
+
+// implicit return(dont use return keyword) 
+// const addTwo = (num1, num2) =>  num1+num2
+
+// const addTwo = (num1, num2) =>  ( num1+num2) 
+
+const addTwo = (num1, num2) =>  ({username:"dipali"}) 
+
+// console.log(addTwo(2,3));
+
+// const myArray = [2, 3, 4, 5]
+
+// myArray.forEach()
