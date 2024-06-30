8f89e6b951b50e8355cae8749c445e92ce4c2e39
Dipali Chormale <116097814+dipalichormale@users.noreply.github.com>
2024-06-30 09:47:49 +0000

    falsy truthy

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,43 @@
+const ussrEmail = []
+
+// if (ussrEmail) {
+//     console.log("got email");
+// } else {
+//     console.log(" dont have user email");
+// }
+
+// falsy values
+// false , 0, -0 , BigInt 0n, "", null, undefined, NaN
+
+// truthy values 
+// "0", 'false', " ", [], {}, function(){}
+
+// if (ussrEmail.length==0) {
+//     console.log("array is empty");
+// }
+
+
+// const emptyObj = {}
+
+// if (Object.keys(emptyObj).length==0) {
+//     console.log("obj is empty");
+// }
+
+
+// nullish coalescing operator (??): null undefined
+
+let val1;
+// val1 = 5 ?? 10
+// val1 = null ?? 10
+// val1 = undefined ?? 15
+// val1 = null ?? 10 ?? 20
+
+// console.log(val1);
+
+
+// ternary operator
+
+// condition ? true : false
+
+const iceTeaPrice = 100
+iceTeaPrice <=80 ? console.log("less 80") : console.log("more 80");
